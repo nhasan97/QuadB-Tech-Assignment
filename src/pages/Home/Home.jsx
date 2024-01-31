a27290322e1e358c8cef01ccfed9a5578f053c8e
nhasan97 <nh6069325@gmail.com>
2024-02-01 00:25:04 +0600

    fetched shows data from api in Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.tvmaze.com/search/shows?q=all")
+      .then((res) => res.json())
+      .then((data) => {
+        setShows(data);
+      });
+  }, []);
+
   return <div></div>;
 };
 
